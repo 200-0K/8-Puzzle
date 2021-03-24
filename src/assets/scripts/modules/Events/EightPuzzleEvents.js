adb5405e0a1a3a9c9333590cd3baf8cf6758c3fd
Faisal S.A <Ksa45560@gmail.com>
2021-03-24 13:31:35 +0300

    [Refactor] EightPuzzleEvents ~ Callback as events parameter + global Duration&Delay

--- src/assets/scripts/modules/Events/EightPuzzleEvents.js
+++ src/assets/scripts/modules/Events/EightPuzzleEvents.js
@@ -1,6 +1,7 @@
 import shuffle from "lodash/shuffle"
 import debounce from "lodash/debounce";
 import throttle from "lodash/throttle";
+import random from "lodash/random";
 
 const classNames = {
     BOARD: "eight-puzzle__board",
@@ -12,14 +13,15 @@ const classNames = {
     RANDOMIZER_ICON: "eight-puzzle__icon", 
 }
 
-// For swapping effect
-let isEffectRunning = false;
+let isEffectRunning   = false; // For swapping effect
+const swapDuration    = 250; // Duration in ms of swapping tiles
+const randomizerDelay = 100; // Delay in ms between swapping tiles
 
 export default class EightPuzzleEvents {
     constructor(onChangeCallBack) {
         this.selectedCells = [];
 
-        // Used after swap 
+        // Used when tiles are changed
         this.onChangeCallBack = onChangeCallBack;
 
         this.event();
@@ -28,18 +30,13 @@ export default class EightPuzzleEvents {
     event() {
         // Swap event
         document.querySelectorAll(`.${classNames.EDITABLE_TABLE}`).forEach(table => table.addEventListener("click", ev => {
-            const isSwipped = this.swapEvent(ev, table);
-            
-            // Call swapCallBack function after swaping
-            if(isSwipped && this.onChangeCallBack instanceof Function) this.onChangeCallBack();
+            this.swapEvent(ev, table, this.onChangeCallBack);
         }));
 
         // Randomizer event
         document.querySelectorAll(`.${classNames.RANDOMIZER_ICON}`).forEach(icon => icon.addEventListener("click", throttle(ev => {
-            this.randomizerEvent(ev);
-
-            if(this.onChangeCallBack instanceof Function) this.onChangeCallBack();
-        }, 500)));
+            this.randomizerEvent(ev, this.onChangeCallBack);
+        }, Math.abs(swapDuration - randomizerDelay) * 5)));
     }
 
     // ************ Events ************ //
@@ -47,14 +44,14 @@ export default class EightPuzzleEvents {
     /**
      * @param {MouseEvent} ev 
      * @param {HTMLElement} table
+     * @param {Function} callback Called after tiles are swapped
      * @returns {boolean} true if swapped, false otherwise
      */
-     swapEvent(ev, table) {
+     swapEvent(ev, table, callback) {
         // Stop user from selecting a tile when effect/swapping is happening
         if (isEffectRunning) return false;
 
         const cellElement = ev.target;
-        let isSwapped     = false;
 
         // If cellElement not a cell or it is a cell but its parent doesn't have EDITABLE_TABLE class name then remove all selections
         if (!cellElement.classList.contains(classNames.CELL)) {
@@ -69,31 +66,37 @@ export default class EightPuzzleEvents {
         else if (table === this.selectedCells[0].closest("table")) {
             this.selectedCells.push(cellElement);
             this.swapTiles();
-            isSwapped = true;
+
+            // callback after swapping tiles
+            if (callback instanceof Function) setTimeout(callback, swapDuration);
         } 
         // If there parents are different then remove all selections
         else this.selectedCells = [];
 
         this.refreshHighlight();
-        return isSwapped;   
     }
 
     /**
      * @param {MouseEvent} ev 
+     * @param {Function} callback Called after tiles are randomized
      */
-    randomizerEvent(ev) {
+    randomizerEvent(ev, callback) {
         const table           = ev.target.closest(`.${classNames.BOARD}`).querySelector("table");
         const cells           = Array.from(table.querySelectorAll(`.${classNames.CELL}`));
         const randomizedCells = shuffle(cells);
         
         const inOrderSwap = setInterval(() => {
             // Swap cells, if one cell left, then swap it with any already swapped cells
-            [this.selectedCells[0], this.selectedCells[1]] = [randomizedCells.pop(), randomizedCells.pop() || cells[Math.floor(Math.random() * cells.length)]];
+            [this.selectedCells[0], this.selectedCells[1]] = [randomizedCells.pop(), randomizedCells.pop() || cells[random(0, cells.length-1)]];
             this.swapTiles();
 
-            // If all cells has been randomized then clear the interval
-            if (randomizedCells.length <= 0) clearInterval(inOrderSwap);
-        }, 200); // delay
+            // If all cells has been randomized then clear the interval &
+            // and call the callback function
+            if (randomizedCells.length <= 0) {
+                clearInterval(inOrderSwap);
+                if(callback instanceof Function) callback();
+            }
+        }, randomizerDelay);
     }
 
     // ************ Methods ************ //
@@ -106,8 +109,10 @@ export default class EightPuzzleEvents {
         if (this.selectedCells.length > 0) this.selectedCells.forEach(cell => cell.classList.add(classNames.SELECTED_CELL));
     }
 
-    // This will use this.selectedCells array to swap tiles
-    swapTiles() {
+    /** This will use this.selectedCells array to swap tiles
+     * @param {Number} effectDuration time in ms to remove swapped class name from swapped cells
+     */
+    swapTiles(effectDuration = swapDuration) {
         const src  = this.selectedCells.pop();
         const dest = this.selectedCells.pop();
         
@@ -127,6 +132,6 @@ export default class EightPuzzleEvents {
             src.classList.remove(`${classNames.SWAPPED_CELL}`);
             dest.classList.remove(`${classNames.SWAPPED_CELL}`);
             isEffectRunning = false;
-        }, 250);
+        }, effectDuration);
     }
 }
